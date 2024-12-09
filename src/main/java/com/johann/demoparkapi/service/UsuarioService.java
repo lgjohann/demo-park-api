ee494240e6dde78e2c19479e3bcc41e6e7b53889
Guilherme Johann <guilherme4695@gmail.com>
2024-12-09 16:42:47 -0300

    Recurso para criação de Usuario

--- src/main/java/com/johann/demoparkapi/service/UsuarioService.java
+++ src/main/java/com/johann/demoparkapi/service/UsuarioService.java
@@ -1,6 +1,8 @@
 package com.johann.demoparkapi.service;
 
+import com.johann.demoparkapi.entity.Usuario;
 import com.johann.demoparkapi.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,9 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
--- src/main/java/com/johann/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/com/johann/demoparkapi/web/controller/UsuarioController.java
@@ -1,9 +1,11 @@
 package com.johann.demoparkapi.web.controller;
 
+import com.johann.demoparkapi.entity.Usuario;
 import com.johann.demoparkapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -11,4 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
+        Usuario user = usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 }
