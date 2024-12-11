c077998008a06f7d0a7df92e12b1f6d43a1154c6
Guilherme Johann <guilherme4695@gmail.com>
2024-12-11 16:24:48 -0300

    Teste ponta a ponta para criar um novo usuário

--- src/test/resources/sql/usuarios/usuarios-insert.sql
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -1,3 +1,3 @@
-INSERT INTO USUARIOS (id, username, password, role)   values (100, "ana@email.com", "123456", "ROLE_ADMIN");
-INSERT INTO USUARIOS (id, username, password, role)   values (101, "bia@email.com", "123456", "ROLE_CLIENTE");
-INSERT INTO USUARIOS (id, username, password, role)   values (102, "bob@email.com", "123456", "ROLE_CLIENTE");
+insert into USUARIOS (id, username, password, role) values (100, 'ana@email.com', '123456', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) values (101, 'bia@email.com', '123456', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values (102, 'bob@email.com', '123456', 'ROLE_CLIENTE');
