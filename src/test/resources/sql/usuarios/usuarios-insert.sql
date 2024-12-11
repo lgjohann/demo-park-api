5c53a439305c135e160d6362e9590c33ea3fcf82
Guilherme Johann <guilherme4695@gmail.com>
2024-12-11 14:22:06 -0300

    Configuração inicial de testes

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO USUARIOS (id, username, password, role)   values (100, "ana@email.com", "123456", "ROLE_ADMIN");
+INSERT INTO USUARIOS (id, username, password, role)   values (101, "bia@email.com", "123456", "ROLE_CLIENTE");
+INSERT INTO USUARIOS (id, username, password, role)   values (102, "bob@email.com", "123456", "ROLE_CLIENTE");
