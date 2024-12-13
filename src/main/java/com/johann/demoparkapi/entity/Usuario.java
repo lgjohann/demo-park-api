35216b450ff89e9d21c0df953ff532625aeaaca0
Guilherme Johann <guilherme4695@gmail.com>
2024-12-13 15:10:38 -0300

    Sistemas de auditoria

--- src/main/java/com/johann/demoparkapi/entity/Usuario.java
+++ src/main/java/com/johann/demoparkapi/entity/Usuario.java
@@ -4,6 +4,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -13,6 +19,7 @@ import java.util.Objects;
 @Getter @Setter @NoArgsConstructor
 @Entity
 @Table(name = "usuarios")
+@EntityListeners(AuditingEntityListener.class)
 public class Usuario implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -31,15 +38,19 @@ public class Usuario implements Serializable {
     @Column(name = "role", nullable = false, length = 25)
     private Role role = Role.ROLE_CLIENTE;
 
+    @CreatedDate
     @Column(name = "data_criacao")
     private LocalDateTime dataCriacao;
 
+    @LastModifiedDate
     @Column(name = "data_modificacao")
     private LocalDateTime dataModificacao;
 
+    @CreatedBy
     @Column(name = "criado_por")
     private String criadoPor;
 
+    @LastModifiedBy
     @Column(name = "modificado_por")
     private String modificadoPor;
 
