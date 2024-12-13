29cb85b179f69f425a5d7b68e184029828691236
Guilherme Johann <guilherme4695@gmail.com>
2024-12-13 15:52:56 -0300

    Configuração de permissao de acesso na buscaPorId

--- src/main/java/com/johann/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/com/johann/demoparkapi/web/controller/UsuarioController.java
@@ -17,6 +17,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -58,6 +59,7 @@ public class UsuarioController {
             }
     )
     @GetMapping(value = "/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
         return ResponseEntity.ok().body(UsuarioMapper.toDto(user));
