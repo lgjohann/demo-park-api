e31d28bcfd93b2e361d8cd969893dce83c05f7ca
Guilherme Johann <guilherme4695@gmail.com>
2024-12-13 13:51:26 -0300

    autenticacaoController e UsuarioLoginDto

--- src/main/java/com/johann/demoparkapi/jwt/JwtUserDetails.java
+++ src/main/java/com/johann/demoparkapi/jwt/JwtUserDetails.java
@@ -1,11 +1,10 @@
 package com.johann.demoparkapi.jwt;
 
 import com.johann.demoparkapi.entity.Usuario;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 
-import java.util.Collection;
+
 
 public class JwtUserDetails extends User {
 
---
+++ src/main/java/com/johann/demoparkapi/web/dto/UsuarioLoginDto.java
@@ -0,0 +1,22 @@
+package com.johann.demoparkapi.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UsuarioLoginDto {
+
+    @NotBlank
+    @Email(regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$",message = "Formato de e-mail inválido")
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
+    private String password;
+}
