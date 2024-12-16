4b59860b87dcefae80ecec02cf26699cfdcea00c
Guilherme Johann <guilherme4695@gmail.com>
2024-12-16 16:51:07 -0300

    ClienteDto e ClienteResponseDto - also mapper

---
+++ src/main/java/com/johann/demoparkapi/web/dto/ClienteResponseDto.java
@@ -0,0 +1,19 @@
+package com.johann.demoparkapi.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ClienteResponseDto {
+
+    private Long Id;
+
+    private String nome;
+
+    private String cpf;
+}
