1b163d0e1400940b083e2ac949631097f745630b
Guilherme Johann <guilherme4695@gmail.com>
2024-12-18 15:23:15 -0300

    Testes com vaga

--- src/main/java/com/johann/demoparkapi/web/controller/VagaController.java
+++ src/main/java/com/johann/demoparkapi/web/controller/VagaController.java
@@ -5,9 +5,17 @@ import com.johann.demoparkapi.service.VagaService;
 import com.johann.demoparkapi.web.dto.VagaCreateDto;
 import com.johann.demoparkapi.web.dto.VagaResponseDto;
 import com.johann.demoparkapi.web.dto.mapper.VagaMapper;
+import com.johann.demoparkapi.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +23,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
+
+@Tag(name = "Vagas", description = "Contém todas as operações relativas ao recurso de uma vaga.")
 @RequestMapping("/api/v1/vagas")
 @RequiredArgsConstructor
 @RestController
@@ -22,6 +32,25 @@ public class VagaController {
 
     private final VagaService vagaService;
 
+    @Operation(
+            summary = "Criar uma nova vaga", description = "Recurso para cirar uma nova vaga." + "Requisição exige o uso de um bearer token. Acesso Restrito para ADMIN.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Recurso criado com sucesso.",
+                            headers = @Header(name = HttpHeaders.LOCATION, description = "URL do recurso criado")),
+                    @ApiResponse(
+                            responseCode = "409",
+                            description = "Vaga já cadastrada.",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "422",
+                            description = "Recurso não processado por falta de dados ou dados inválidos.",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<Void> create(@RequestBody @Valid VagaCreateDto vagaCreateDto) {
@@ -34,6 +63,21 @@ public class VagaController {
         return ResponseEntity.created(location).build();
     }
 
+
+    @Operation(
+            summary = "Localizar uma vaga", description = "Recurso para localizar uma vaga." + "Requisição exige o uso de um bearer token. Acesso Restrito para ADMIN.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Recurso localizado com sucesso.",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = VagaResponseDto.class))),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Vaga não encontrada.",
+                            content = @Content(mediaType = "application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+            })
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{codigo}")
     public ResponseEntity<VagaResponseDto> getByCodigo(@PathVariable String codigo) {
---
+++ src/test/resources/sql/vagas/vagas-insert.sql
@@ -0,0 +1,9 @@
+insert into USUARIOS (id, username, password, role) values (100, 'ana@email.com', '$2a$12$YYRiTYc2ssBR1xFZy4Xm7O1ThyvUgtMFa224k1b9Ywru/a.Z7IeNK', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) values (101, 'bia@email.com', '$2a$12$YYRiTYc2ssBR1xFZy4Xm7O1ThyvUgtMFa224k1b9Ywru/a.Z7IeNK', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values (102, 'bob@email.com', '$2a$12$YYRiTYc2ssBR1xFZy4Xm7O1ThyvUgtMFa224k1b9Ywru/a.Z7IeNK', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values (103, 'toby@email.com', '$2a$12$YYRiTYc2ssBR1xFZy4Xm7O1ThyvUgtMFa224k1b9Ywru/a.Z7IeNK', 'ROLE_CLIENTE');
+
+insert into VAGAS (id, codigo, status) values (10, 'A-01', 'LIVRE');
+insert into VAGAS (id, codigo, status) values (20, 'A-02', 'LIVRE');
+insert into VAGAS (id, codigo, status) values (30, 'A-03', 'OCUPADA');
+insert into VAGAS (id, codigo, status) values (40, 'A-04', 'LIVRE');
