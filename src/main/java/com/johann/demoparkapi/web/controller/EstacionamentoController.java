4f16b8908f88b3309505673fc26c6d36e17fd3a2
Guilherme Johann <guilherme4695@gmail.com>
2024-12-19 14:42:37 -0300

    EstacionamentoController e metodo checkIn

---
+++ src/main/java/com/johann/demoparkapi/web/controller/EstacionamentoController.java
@@ -0,0 +1,40 @@
+package com.johann.demoparkapi.web.controller;
+
+import com.johann.demoparkapi.entity.ClienteVaga;
+import com.johann.demoparkapi.service.EstacionamentoService;
+import com.johann.demoparkapi.web.dto.EstacionamentoCreateDto;
+import com.johann.demoparkapi.web.dto.EstacionamentoResponseDto;
+import com.johann.demoparkapi.web.dto.mapper.ClienteVagaMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("api/v1/estacionamentos")
+@RequiredArgsConstructor
+public class EstacionamentoController {
+
+    private final EstacionamentoService estacionamentoService;
+
+    @PostMapping("/check-in")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<EstacionamentoResponseDto> checkIn(@RequestBody @Valid EstacionamentoCreateDto dto) {
+        ClienteVaga clienteVaga = ClienteVagaMapper.toClienteVaga(dto);
+        estacionamentoService.checkIn(clienteVaga);
+        EstacionamentoResponseDto responseDto = ClienteVagaMapper.toDto(clienteVaga);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequestUri().path("/{recibo}")
+                .buildAndExpand(clienteVaga.getRecibo())
+                .toUri();
+        return ResponseEntity.created(location).body(responseDto);
+    }
+
+}
