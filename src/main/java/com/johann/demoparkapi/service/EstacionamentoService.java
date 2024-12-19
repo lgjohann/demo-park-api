6f7d5cbbf2429d3adccdbfe9af50c1f515ae9a4f
Guilherme Johann <guilherme4695@gmail.com>
2024-12-19 14:35:03 -0300

    Regra de negócio para salvar informações do veículo estacionado

--- src/main/java/com/johann/demoparkapi/service/EstacionamentoService.java
+++ src/main/java/com/johann/demoparkapi/service/EstacionamentoService.java
@@ -1,7 +1,14 @@
 package com.johann.demoparkapi.service;
 
+import com.johann.demoparkapi.entity.Cliente;
+import com.johann.demoparkapi.entity.ClienteVaga;
+import com.johann.demoparkapi.entity.Vaga;
+import com.johann.demoparkapi.util.EstacionamentoUtils;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +17,20 @@ public class EstacionamentoService {
     private final ClienteVagaService clienteVagaService;
     private final ClienteService clienteService;
     private final VagaService vagaService;
+
+    @Transactional
+    public ClienteVaga checkIn(ClienteVaga clienteVaga) {
+        Cliente cliente = clienteService.buscarPorCpf(clienteVaga.getCliente().getCpf());
+        clienteVaga.setCliente(cliente);
+
+        Vaga vaga = vagaService.buscarPorVagaLivre();
+        vaga.setStatus(Vaga.StatusVaga.OCUPADA);
+        clienteVaga.setVaga(vaga);
+
+        clienteVaga.setDataEntrada(LocalDateTime.now());
+
+        clienteVaga.setRecibo(EstacionamentoUtils.gerarRecibo());
+
+        return clienteVagaService.salvar(clienteVaga);
+    }
 }
