7b9c79d02ee8688f58fbfe45b976076323c5efdc
Guilherme Johann <guilherme4695@gmail.com>
2024-12-19 14:07:07 -0300

    EstacionamentoUtils e gerarRecibo

---
+++ src/main/java/com/johann/demoparkapi/util/EstacionamentoUtils.java
@@ -0,0 +1,23 @@
+package com.johann.demoparkapi.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EstacionamentoUtils {
+
+
+    // 2023-03-16T15:23:48.615454232
+    // 20230316-152121
+
+    public static String gerarRecibo() {
+        LocalDateTime date = LocalDateTime.now();
+        String recibo = date.toString().substring(0, 19);
+        return recibo
+                .replace("-", "")
+                .replace(":", "")
+                .replace("T", "");
+    }
+}
